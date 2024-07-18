62a8a41b22063067beb3da7623a1e7cdf774e08f
diegoh86 <47900584+diegoh86@users.noreply.github.com>
2024-07-18 10:13:35 -0300

    Add files via upload

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,13 @@
+class EmailsController < ApplicationController
+  def send_email
+
+    email_obj = {}
+    email_obj[:nome] = params[:nome]
+    email_obj[:email] = params[:email]
+    email_obj[:telefone] = params[:telefone]
+    email_obj[:mensagem] = params[:mensagem]
+
+    ContactMailer.contact_email(email_obj).deliver_now
+    render json: { message: 'Email enviado com sucesso' }
+  end
+end
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,8 @@
+class ContactMailer < ApplicationMailer
+  def contact_email(params)
+
+    txt_email = "Nome: #{params[:nome]}\nEmail: #{params[:email]}\nTelefone: #{params[:telefone]}\nMensagem: #{params[:mensagem]}"
+
+    mail(to: ENV['CONTACT_EMAIL'], subject: 'Mensagem de contato do site', from: ENV['ZOHO_EMAIL'], reply_to: params[:email], body: txt_email)
+  end
+end
