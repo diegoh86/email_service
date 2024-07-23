26d619447084a6336ad4943dfe3b25cc05015ba2
diegoh86 <47900584+diegoh86@users.noreply.github.com>
2024-07-22 21:46:52 -0300

    Add files via upload

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,6 +5,7 @@ class EmailsController < ApplicationController
     email_obj[:nome] = params[:nome]
     email_obj[:email] = params[:email]
     email_obj[:telefone] = params[:telefone]
+    email_obj[:empresa] = params[:empresa]
     email_obj[:mensagem] = params[:mensagem]
 
     ContactMailer.contact_email(email_obj).deliver_now
