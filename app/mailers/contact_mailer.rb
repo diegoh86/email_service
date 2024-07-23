06babaa44b4732ec655a2c770a27a151de0261c1
diegoh86 <47900584+diegoh86@users.noreply.github.com>
2024-07-22 21:47:24 -0300

    Add files via upload

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact_email(params)
 
-    txt_email = "Nome: #{params[:nome]}\nEmail: #{params[:email]}\nTelefone: #{params[:telefone]}\nMensagem: #{params[:mensagem]}"
+    txt_email = "Nome: #{params[:nome]}\nEmail: #{params[:email]}\nTelefone: #{params[:telefone]}\nEmpresa: #{params[:empresa]}\nMensagem: #{params[:mensagem]}"
 
     mail(to: ENV['CONTACT_EMAIL'], subject: 'Mensagem de contato do site', from: ENV['ZOHO_EMAIL'], reply_to: params[:email], body: txt_email)
   end
